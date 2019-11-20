6792922b5a32222b5b9f2bd7652a7ac28c832229
scareoset <daniel.wright.314@gmail.com>
2019-11-20 13:23:42 -0800

    enemy go fast

--- PrototypeUno/Assets/Scripts/SafeLight.cs
+++ PrototypeUno/Assets/Scripts/SafeLight.cs
@@ -10,6 +10,11 @@ public class SafeLight : MonoBehaviour {
 
   // Update is called once per frame
   void Update() {
-
+    var transform = this.GetComponent<Transform>();
+    transform.Translate(
+      1.0f * Random.Range(-0.25f, 0.25f),
+      0.0f,
+      1.0f * Random.Range(-0.25f, 0.25f)
+    );
   }
 }
