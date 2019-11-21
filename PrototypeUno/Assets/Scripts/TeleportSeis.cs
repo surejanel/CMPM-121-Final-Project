76baed6d95c3b90ac456694f15d30c33e2b8df7d
scareoset <daniel.wright.314@gmail.com>
2019-11-21 12:54:22 -0800

    nice and proper teleport in all directions

--- PrototypeUno/Assets/Scripts/TeleportSeis.cs
+++ PrototypeUno/Assets/Scripts/TeleportSeis.cs
@@ -1,8 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeleportDos : MonoBehaviour {
+public class TeleportSeis : MonoBehaviour {
   // Start is called before the first frame update
   void Start() {
 
@@ -17,7 +17,7 @@ public class TeleportDos : MonoBehaviour {
     if(other.tag == "Player") {
       CharacterController player = other.GetComponent<CharacterController>();
       var transform = other.GetComponent<Transform>();
-      Vector3 newPosition = new Vector3(0.0f, 2.0f, 195.0f);
+      Vector3 newPosition = new Vector3(0.0f, 2.0f, 210.0f);
       player.enabled = false;
       transform.position = newPosition;
       player.enabled = true;
