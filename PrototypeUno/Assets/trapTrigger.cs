41b9e8b2d6dab0dd07df6391fef212bc2d83ce34
scareoset <daniel.wright.314@gmail.com>
2019-12-03 13:35:54 -0800

    :< its not WORKING SDFKGSDF

--- PrototypeUno/Assets/trapTrigger.cs
+++ PrototypeUno/Assets/trapTrigger.cs
@@ -5,52 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class trapTrigger : MonoBehaviour
 {
-    public GameObject pressurePlate;
-    public GameObject trap;
-    Transform trapTransform;
+  public GameObject pressurePlate;
+  public GameObject trap;
+  Transform trapTransform;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-      trapTransform = trap.GetComponent<Transform>();
+  // Start is called before the first frame update
+  void Start()
+  {
+    trapTransform = trap.GetComponent<Transform>();
 
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+
+	  // Make the scale on the Y increase.
+   	// Then make it so if you connect with the hidden cube, the scene resets. Using these two lines:
+	  PlayerDeathSFX.PlayerDeathSound.Play();
+
+    //spike go up
+    trapTransform.position = Vector3.Lerp(trapTransform.position, new Vector3(trapTransform.position.x, 1, trapTransform.position.z), 1);
+
+    //reload when the player collides with it
+    if(other.gameObject.CompareTag("Player")) {
+      Debug.Log("ur dead bich");
+      // SceneManager.LoadScene("Prototype One");
+    } else {
+      Debug.Log("fuck");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
+    if(other.gameObject.CompareTag("Monster")) {
+      Debug.Log("got em (the monster)" );
+      other.gameObject.SetActive(false);
     }
-
-    void OnTriggerEnter(Collider other)
-	  {
-
-		  // Make the scale on the Y increase.
-	   	// Then make it so if you connect with the hidden cube, the scene resets. Using these two lines:
-		  PlayerDeathSFX.PlayerDeathSound.Play();
-
-      //spike go up
-      trapTransform.position = Vector3.Lerp(
-        trapTransform.position,
-        new Vector3(
-          trapTransform.position.x,
-          1,
-          trapTransform.position.z
-        ),
-        1
-      );
-
-       //reload when the player collides with it
-       if(other.gameObject.CompareTag("Player")) {
-         SceneManager.LoadScene("Prototype One");
-       }
-
-       if(other.gameObject.CompareTag("Monster")) {
-         Debug.Log("got em (the monster)" );
-         other.gameObject.SetActive(false);
-       }
-
-
-	  }
-
+  }
 }
