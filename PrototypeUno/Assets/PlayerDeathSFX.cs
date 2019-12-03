872c78eed32e3a1c28f335b815f333016a215a8a
scareoset <daniel.wright.314@gmail.com>
2019-12-03 10:58:18 -0800

    break over

---
+++ PrototypeUno/Assets/PlayerDeathSFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeathSFX : MonoBehaviour
+{
+	public static AudioSource PlayerDeathSound;
+	// Start is called before the first frame update
+	void Start()
+	{
+		PlayerDeathSound = GetComponent<AudioSource>();
+		//footstepSound.Play();
+		//footstepSound.volume = Random.Range(0.2f, .5f);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//footstepSound.Play();
+		//footstepSound.volume = Random.Range(0.2f, .5f);
+	}
+}
---
+++ PrototypeUno/Assets/trapTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class trapTrigger : MonoBehaviour
+{
+    public GameObject pressurePlate;
+    public GameObject trap;
+    Transform trapTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      trapTransform = trap.GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter()
+	  {
+
+		// Make the scale on the Y increase.
+		// Then make it so if you connect with the hidden cube, the scene resets. Using these two lines:
+		 PlayerDeathSFX.PlayerDeathSound.Play();
+
+     //spike go up
+     trapTransform.position = Vector3.Lerp( trapTransform.position,
+                                            new Vector3(trapTransform.position.x, 1, trapTransform.position.z),
+                                            1);
+
+	  }
+
+}
